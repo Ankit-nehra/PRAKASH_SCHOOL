155e302a2f7e8c5a3a3b392572d0ce07a4032487
Ankit-nehra <ankitnehra7000@gmail.com>
2026-03-17 13:00:39 +0530

    Update server.js

--- school/server/server.js
+++ school/server/server.js
@@ -1,8 +1,6 @@
-// server/server.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -15,21 +13,11 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "../uploads"))); // for uploaded files
+app.use("/uploads", express.static("uploads"));
 
-// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/gallery", galleryRoutes);
 app.use("/api/notices", noticeRoutes);
 
-// Serve Vite build (frontend)
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "../dist")));
-
-// SPA fallback: serve index.html for all other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
