d83b17cc325a967542ddaae9d8208981473fc12d
Ankit-nehra <ankitnehra7000@gmail.com>
2026-07-06 01:13:41 +0530

    Delete school/src directory

--- school/src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-// src/components/Loader.js
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
-
-// Configure NProgress
-NProgress.configure({ showSpinner: false, trickleSpeed: 200 });
-
-export const startLoading = () => NProgress.start();
-export const stopLoading = () => NProgress.done();
