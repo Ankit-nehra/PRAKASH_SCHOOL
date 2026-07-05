f9f0e15e3fa54b44ebb0b0d3f75e12b2e1cc26e9
Ankit-nehra <ankitnehra7000@gmail.com>
2026-07-06 00:47:48 +0530

    Delete school/server directory

--- school/server/models/Gallery.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const gallerySchema = new mongoose.Schema({
-  image: String,
-  category: String,
-});
-
-export default mongoose.model("Gallery", gallerySchema);
