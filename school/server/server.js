8b7508638098461fab05b189e78b8bc6e5c9b61e
Ankit-nehra <ankitnehra7000@gmail.com>
2026-07-06 00:50:10 +0530

    Add files via upload

---
+++ school/server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import galleryRoutes from "./routes/galleryRoutes.js";
+import noticeRoutes from "./routes/noticeRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/gallery", galleryRoutes);
+app.use("/api/notices", noticeRoutes);
+
+// Default port fallback
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
