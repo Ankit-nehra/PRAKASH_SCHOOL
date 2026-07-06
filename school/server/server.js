ab36d0e38684b6a322e5629fda23cdcc2177a8e7
Ankit-nehra <ankitnehra7000@gmail.com>
2026-07-06 10:28:14 +0530

    Update server.js

--- school/server/server.js
+++ school/server/server.js
@@ -7,7 +7,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import galleryRoutes from "./routes/galleryRoutes.js";
 import noticeRoutes from "./routes/noticeRoutes.js";
-
+import achievementRoutes from "./routes/achievementRoutes.js";
 dotenv.config();
 connectDB();
 
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/gallery", galleryRoutes);
 app.use("/api/notices", noticeRoutes);
-
+app.use("/api/achievements", achievementRoutes);
 // Default port fallback
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
